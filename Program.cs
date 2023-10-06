aeb8c439adbb78dc2a3320c7d680bf4c2a9e4669
Jean Tourloupe <134067308+Cramptiste@users.noreply.github.com>
2023-10-06 22:24:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+using SentinelSyncV1;
+
+namespace SentinelSyncV1
+{
+    class Program
+    {
+        static string RefreshCpuInfos()
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter();
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+            dynamic firstVal = cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(100);
+            dynamic val = cpuCounter.NextValue();
+
+            decimal roundVal = Convert.ToDecimal(val);
+            roundVal = Math.Round(roundVal, 2);
+
+            return roundVal + " %";
+        }
+        static void GetAllSystemInfos()
+        {
+            var si = new SystemInfo();
+            Console.WriteLine(si.GetCpuInfos());
+            Console.WriteLine(si.GetOsInfos("os"));
+            Console.WriteLine(si.GetOsInfos("arch"));
+            Console.WriteLine(si.GetOsInfos("osv"));
+            si.GetGpuInfos();
+        }
+        static void ShowCpuUse()
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    return;
+                }
+                Console.WriteLine("(ENTER to stop) processor utilisation : " + RefreshCpuInfos());
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            GetAllSystemInfos();
+            int choice = 0;
+
+            Console.WriteLine("1 - show processor utilisation");
+            Console.WriteLine("2 - show RAM utilisation");
+            Console.WriteLine("3 - show PC temperature");
+            Console.WriteLine("4 - show stockage infos");
+            Console.WriteLine("5 - connexion infos");
+
+            choice = outils.AskNumberBetween("enter a choice : ", 1, 5);
+            switch (choice)
+            {
+                case 1:
+                    ShowCpuUse();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+
+            }
+        }
+    }
+}
---
+++ SystemInfo.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentinelSyncV1
+{
+    public class SystemInfo
+    {
+        // getting OS informations
+        public string GetOsInfos(string param)
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+            // mo = management object
+            foreach (ManagementObject? mo in mos.Get())
+            {
+                switch (param)
+                {
+                    case "os":
+                        return mo["Caption"]?.ToString() ?? "";
+                    // Arch = architecture
+                    case "arch":
+                        return mo["OSArchitecture"]?.ToString() ?? "";
+                    // Osv = version du système d'exploitation
+                    case "osv":
+                        return mo["CSDVersion"]?.ToString() ?? "";
+                }
+            }
+            return "";
+        }
+        // getting CPU infos
+        public string GetCpuInfos()
+        {
+            RegistryKey processor_name = Registry.LocalMachine.OpenSubKey(
+                @"Hardware\Description\System\CentralProcessor\0",
+                RegistryKeyPermissionCheck.ReadSubTree
+                );
+            if (processor_name != null)
+            {
+                return processor_name.GetValue("ProcessorNameString").ToString();
+            }
+            return "";
+        }
+
+        // getting GPU infos
+        public void GetGpuInfos()
+        {
+            using (var searcher = new ManagementObjectSearcher("select * from Win32_Videocontroller"))
+            {
+                foreach (ManagementObject? obj in searcher.Get())
+                {
+                    Console.WriteLine("Name - " + obj["Name"]);
+                    Console.WriteLine("DeviceID - " + obj["DeviceID"]);
+                    Console.WriteLine("AdapterRAM - " + obj["AdapterRAM"]);
+                    Console.WriteLine("AdapterDACType - " + obj["AdapterDACType"]);
+                    Console.WriteLine("Monochrome - " + obj["Monochrome"]);
+                    Console.WriteLine("InstalledDisplayDrivers - " + obj["InstalledDisplayDrivers"]);
+                    Console.WriteLine("DriverVersion - " + obj["DriverVersion"]);
+                    Console.WriteLine("VideoProcessor - " + obj["VideoProcessor"]);
+                    Console.WriteLine("VideoArchitecture - " + obj["VideoArchitecture"]);
+                    Console.WriteLine("VideoMemoryType - " + obj["VideoMemoryType"]);
+                }
+            }
+        }
+
+    }
+
+}
